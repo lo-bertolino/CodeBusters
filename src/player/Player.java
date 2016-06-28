adb64437adfe140f04f88638199e9bd3a67c3156
lo98be <bertol.lollo@outlook.com>
2016-06-29 11:06:52 +1200

    Miglioramento messaggi di errore per faciltare il debug

--- src/player/Player.java
+++ src/player/Player.java
@@ -2,6 +2,7 @@
 import java.util.*;
 
 class Entity {
+	public static int teamId; // # of busters * player
 	public int id;
 	public int x;
 	public int y;
@@ -19,14 +20,30 @@ class Entity {
 		this.value = copied.value;
 	}
 	public Entity (int x,int y){
+		this.id=0;
 		this.x=x;
 		this.y=y;
+		this.type = 0;
+		this.state = 0;
+		this.value = 0;
 	}
 	public int  entDist (Entity b){
 		return (int) Math.sqrt((this.x - b.x)*(this.x-b.x)+(this.y-b.y)*(this.y-b.y));
 	}
 	public void print(){
-		System.err.printf("%d %d %d %d %d %d ",this.id,this.x,this.y,this.type,this.state,this.value);
+		if (this.type==teamId){
+			System.err.printf("My buster is #d and is in %d %d,",type,this.id,this.x,this.y);
+			if (this.state==1){
+				System.err.printf("this buster is carrying ghost #%d\n",this.value);
+			}else{
+				System.err.printf("this buster isn't carrying any ghost\n");
+			}
+		}else{
+			System.err.printf("This ghost is # %d and is in %d %d\n",type,this.id,this.x,this.y);
+			if (this.value!=0){
+				System.err.printf("This ghost is currenty targeted by %d buster(s)\n",this.value);
+			}
+		}
 	}
 	public void copy(Entity copied){
 		this.id = copied.id;
@@ -41,15 +58,14 @@ class Entity {
 class Player {
     public static void main(String[] args) {
         Scanner cin  = new Scanner(System.in);
-		int teamId; // # of busters * player
 		int ghostCount; //# of ghosts in map
 		int bustersPP; //player 0 or 1
 		bustersPP = cin.nextInt();
 		ghostCount = cin.nextInt();
-		teamId = cin.nextInt();
-		System.err.printf("%d %d %d\n",bustersPP,ghostCount,teamId);
+		Entity.teamId = cin.nextInt();
+		System.err.printf("%d %d %d\n",bustersPP,ghostCount,Entity.teamId);
 		Entity base,dest;
-		if (teamId==0){
+		if (Entity.teamId==0){
 			base = new Entity(0,0);
 			dest = new Entity(16001,9001);
 		}else{
@@ -74,7 +90,7 @@ class Player {
 				temp.type=cin.nextInt();
 				temp.state=cin.nextInt();
 				temp.value=cin.nextInt();
-				if (temp.type==teamId){
+				if (temp.type==Entity.teamId){
 					buster[busters] = new Entity (temp);
 					busters++;
 				}else if (temp.type==-1){
@@ -83,7 +99,14 @@ class Player {
 				}
 			}
 			/*debug*/
-			System.err.printf("# busters:%d\n# ghosts:%d\n",busters,ghosts);
+			System.err.printf("# busters:%d\n",busters);
+			for (int i = 0;i < busters;i++){
+				buster[i].print();
+			}
+			System.err.printf("# ghosts:%d\n",ghosts);
+			for (int i = 0;i < ghosts;i++){
+				ghost[i].print();
+			}
 			/*debug*/
 			//operations
 			for (int i = 0;i < busters;i++){
@@ -91,24 +114,25 @@ class Player {
 					if (buster[i].entDist(base)<1600){
 						System.out.println("RELEASE\n");
 					}else{
-						System.out.printf("MOVE %d %d\n",base.x,base.y);
+						System.out.printf("MOVE %d %d home %d\n",base.x,base.y,buster[i].value);
 					}
 				}
-				boolean found=false;
-		    	for (int j = 0;j < ghosts;j++){
-		    		if (ghost[j].value!=0){ continue;}
-					int dist = buster[i].entDist(ghost[j]);
-					if (dist<1760){
-			    		System.out.printf("BUST %d\n",ghost[j].id);
-				    	found=true;
-				    }else if (dist<2200){
-				    	System.out.printf("MOVE %d %d\n",ghost[j].x,ghost[j].y);
-				    	System.err.printf("%d %d\n",ghost[j].id,dist);
-				    	found=true;
+				else{
+					boolean found=false;
+					for (int j = 0;j < ghosts;j++){
+						if (ghost[j].value!=0){ continue;}
+						int dist = buster[i].entDist(ghost[j]);
+						if (dist<1760){
+							System.out.printf("BUST %d\n",ghost[j].id);
+					    	found=true;
+					    }else if (dist<2200){
+					    	System.out.printf("MOVE %d %d, %d found\n",ghost[j].x,ghost[j].y,ghost[j].value);
+					    	found=true;
+						}
 					}
-				}
-				if (!found){
-					System.out.printf("MOVE %d %d\n",dest.x,dest.y);
+					if (!found){
+						System.out.printf("MOVE %d %d nobody\n",dest.x,dest.y);
+					}
 				}
 			}
 		}
